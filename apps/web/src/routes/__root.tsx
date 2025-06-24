915910d19d2c247aff4eacadaf5f1d488ef6b12b
ahargunyllib <billy.bpm03@gmail.com>
2025-06-24 15:20:41 +0700

    style: format CSS and TypeScript files for improved readability

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -36,6 +36,19 @@ export const Route = createRootRouteWithContext<RouterAppContext>()({
 				rel: "icon",
 				href: "/favicon.ico",
 			},
+			{
+				rel: "preconnect",
+				href: "https://fonts.googleapis.com",
+			},
+			{
+				rel: "preconnect",
+				href: "https://fonts.gstatic.com",
+				crossOrigin: "anonymous",
+			},
+			{
+				rel: "stylesheet",
+				href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Lusitana:wght@400;700&display=swap",
+			},
 		],
 	}),
 });
