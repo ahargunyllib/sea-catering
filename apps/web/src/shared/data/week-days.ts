492c4511ec24d57c86685e76f22a3df3270b4160
ahargunyllib <billy.bpm03@gmail.com>
2025-07-01 18:39:56 +0700

    feat(week-days): add week days data structure with indices and short names

---
+++ apps/web/src/shared/data/week-days.ts
@@ -0,0 +1,9 @@
+export const WeekDays = [
+	{ idx: 0, name: "Sunday", short: "Sun" },
+	{ idx: 1, name: "Monday", short: "Mon" },
+	{ idx: 2, name: "Tuesday", short: "Tue" },
+	{ idx: 3, name: "Wednesday", short: "Wed" },
+	{ idx: 4, name: "Thursday", short: "Thu" },
+	{ idx: 5, name: "Friday", short: "Fri" },
+	{ idx: 6, name: "Saturday", short: "Sat" },
+];
